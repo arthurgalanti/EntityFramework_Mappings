298a71fc5205ab60d9f4c58f58e28a42419a979f
ArthurGalanti <arthur.galanti@hotmail.com>
2023-01-27 16:36:35 -0300

    Adicionado DB Context

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
-﻿internal class Program
+﻿using BALTA.IO_FundamentosEF.Data;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        using(var context = new BlogDataContext())
+        {
+
+        }
+
     }
 }
