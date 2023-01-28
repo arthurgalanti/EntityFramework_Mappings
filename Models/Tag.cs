fc29dc2376eb54d654110b76bb791e34e177e8d5
ArthurGalanti <arthur.galanti@hotmail.com>
2023-01-28 08:38:23 -0300

    Adicionado mapeamento com Data Notations

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BALTA.IO_FundamentosEF.Models
 {
+    [Table("Tag")]
     public class Tag
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }   
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using BALTA.IO_FundamentosEF.Data;
+using BALTA.IO_FundamentosEF.Models;
+using Microsoft.EntityFrameworkCore;
 
 internal class Program
 {
@@ -6,8 +8,28 @@ internal class Program
     {
         using(var context = new BlogDataContext())
         {
+            // var tag = new Tag { Name= "TesteEF", Slug="test"};
+            // context.Tags.Add(tag);
+            // context.SaveChanges();
 
+            // var tag = context.Tags.AsNoTracking().FirstOrDefault(x=> x.Id == 2);
+            // Console.WriteLine(tag?.Name);
+
+            // var tag = context.Tags.FirstOrDefault(x=> x.Id == 2);
+            // tag.Name = ".NET";
+            // tag.Slug = "dotnet";
+
+            // context.Update(tag);
+            // context.SaveChanges();
+
+            //  var tag = context.Tags.FirstOrDefault(x=> x.Id == 16);
+            // context.Remove(tag);
+            // context.SaveChanges();
+
+            // var tags = context.Tags.ToList();
+
+            // foreach (var tag in tags)
+            //     Console.WriteLine(tag.Name);
         }
-
     }
 }
